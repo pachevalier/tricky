93a589ddffda297651f5398a9c6e4c776ffb914e
Paul-Antoine <paul-antoine.chevalier@data.gouv.fr>
2015-10-21 17:47:30 +0200

    adding a readme and a new function

---
+++ R/percent_formatting.R
@@ -0,0 +1,11 @@
+#' Formatting percentages
+#'
+#' This function allows you to format percentages
+#' @param x the number you need to format
+#' @keywords format
+#' @export
+#' @examples
+#' percent_formatting(0.012)
+#'
+
+percent_formatting <- function(x) paste(100*x, "%", sep = " ")
---
+++ Readme.md
@@ -0,0 +1,12 @@
+
+## Tricky
+
+Some useful R functions
+
+* `french_formatting()` 
+
+## Tips
+
+* [Hilary Parker's guide](http://hilaryparker.com/2014/04/29/writing-an-r-package-from-scratch/)
+
+
