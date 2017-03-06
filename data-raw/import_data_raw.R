707f76a4aaf6293b8c9f29cbbdf7a9b4376b704b
Paul-Antoine <paul-antoine.chevalier@data.gouv.fr>
2017-03-06 16:47:08 +0100

    changing directory for external data file following hadley's conventions (R packages books) + improving vignettes by removing local paths to datasets + generating the documentation with pkgdown package

--- data-raw/import_data_raw.R
+++ data-raw/import_data_raw.R
@@ -2,10 +2,10 @@ library("curl")
 
 curl_download(
   url = "https://www.data.gouv.fr/s/resources/listes-de-personnalites-issues-de-wikidata-1/20160905-165452/deputes.csv",
-  destfile = "data-raw/table_deputes.csv"
+  destfile = "inst/extdata/table_deputes.csv"
   )
 
 curl_download(
   url = "http://www.data.gouv.fr/fr/datasets/r/54aa0d23-d00e-4cdd-ba33-62299b64199d",
-  destfile = "data-raw/DINSIC-Panorama_des_grands_projets_SI_20161116.xlsx"
+  destfile = "inst/extdata/DINSIC-Panorama_des_grands_projets_SI_20161116.xlsx"
   )
