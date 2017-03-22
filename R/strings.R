8771058963efb2bd5f16b6a72e39bf6229c786a4
paul-antoine <paul-antoine.chevalier@data.gouv.fr>
2017-03-22 10:57:51 +0100

    docs directory is ignored in building the package \n str_standardize use stringi to remove non ascii caracters \n checking is OK

--- R/strings.R
+++ R/strings.R
@@ -2,6 +2,8 @@
 #'
 #' Standardize removes accents, blank spaces, special caracters from a string
 #'
+#' See http://stackoverflow.com/a/36898175/1967500 for the tricks to remove accents
+#'
 #' @param string a string
 #' @param prefix a string
 #'
@@ -10,29 +12,16 @@
 #'
 #' @examples
 #' str_standardize("2017/07/07", prefix = "date_")
-#' str_standardize(stringi::stri_trans_general("code externe de l'action", "latin-ascii"))
+#' str_standardize("code externe de l'action")
 #'
 str_standardize <- function(string, prefix = "var_") {
   string %>%
     stringr::str_trim(
       side = "both") %>%
     stringr::str_to_lower() %>%
-    stringr::str_replace_all(
-      pattern = "[ç]",
-      replacement = "c") %>%
-    stringr::str_replace_all(
-      pattern = "[éèê]",
-      replacement = "e") %>%
-    stringr::str_replace_all(
-      pattern = "[âà]",
-      replacement = "a") %>%
-    stringr::str_replace_all(
-      pattern = "[ùû]",
-      replacement = "u") %>%
-    stringr::str_replace_all(
-      pattern = "[ô]",
-      replacement = "o"
-    ) %>%
+    stringi::stri_trans_general(
+      id = "Latin-ASCII"
+      ) %>%
     stringr::str_replace_all(
       pattern = "[[:blank:]\\'\\-\\/]",
       replacement = "_"
