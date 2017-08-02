d00d8284a8fd7cbcd00ce0b274b94f2c454f5391
Paul-Antoine <pachevalier@gmail.com>
2017-08-02 14:09:33 +0200

    new function to detect siren using tidyeval

--- R/datadetective.R
+++ R/datadetective.R
@@ -1,60 +1,29 @@
+library(magrittr)
+library(rlang)
 
-#' is SIREN
-#'
-#' is_siren detect if the variable match the pattern of a SIRENE number
-#'
-#' @param var name of a variable
-#'
-#' @return a tibble with one boolean column
-#' @export
-#'
-#' @examples
-#' library("tibble")
-#' table_datadetective <- tibble(
-#'    v1 = c("123456789", "234567890"),
-#'    v2 = c("1234567890123", "1234567890234")
-#'    )
-#'
-#' is_siren(var = table_datadetective$v1)
-#' is_siren(var = table_datadetective$v2)
-#'
-is_siren <- function(var) {
-  tibble::tibble(x = all(
-    stringr::str_detect(
-      string = var,
-      pattern = "^[[:digit:]]{9}$"
-      )
-   )
-  )
-}
-
-#' Detect Siren
-#'
-#' detects if a data frame has a column of SIRENE identifier
-#'
-#' @param .data a data frame
-#'
-#' @return a tibble
-#' @export
-#'
-#' @examples
-#'
-#' library("tibble")
-#'
-#' table_datadetective <- tibble::tibble(
-#'   v1 = c("123456789", "234567890"),
-#'   v2 = c("1234567890123", "1234567890234")
-#'   )
-#'
-#' detect_siren(.data = table_datadetective)
-#'
-detect_siren <- function(.data) {
-
+detect_siren <- function(tbl) {
   plyr::ldply(
-    .data = .data %>%
-      dplyr::select_if(is.character),
-    .fun = is_siren
+    .data = dplyr::select_if(.tbl = tbl, .predicate = is.character),
+    .fun = function(x) {
+      all(
+        stringr::str_detect(
+        string = x,
+        pattern = "^[[:digit:]]{9}$"
+        )
+      )
+      }
+    ) %>%
+  dplyr::filter(!! quo(V1 == TRUE)) %>%
+  dplyr::rename(
+    "column" := .id,
+    "is_siren" := V1
   ) %>%
-    dplyr::filter_(.dots = ~ x == TRUE)
+  tibble::as_tibble()
 
 }
+
+tbl_datadetective <- tibble::tibble(
+  x = c("123456789", "234567890"),
+  y = c("1234567890123", "1234567890234")
+  )
+detect_siren(tbl = tbl_datadetective)
